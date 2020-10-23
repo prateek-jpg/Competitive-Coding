8baa62e2e04898505e37082eddd08f4834eb921c
preetinair <30437969+preetinair@users.noreply.github.com>
2020-10-23 13:07:51 +0530

    Create ConstructorInJava.java
    By Preeti Nair

---
+++ ConstructorInJava.java
@@ -0,0 +1,15 @@
+public class HelloWorld{
+    HelloWorld(){
+         System.out.println("Inside Parent class");
+    }
+
+    
+    public static void main(String []args){
+        System.out.println("Starting Execution");
+        HelloWorld obj=new HelloWorld();
+     }
+
+     
+}
+
+
